922709cafef4802c560582c1e06a26f397e768c6
Fernando de Abreu <fernandoengenharia11@gmail.com>
2020-11-25 18:38:18 -0300

    Teste JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-alert("meu primeiro js")
+/*var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+/*for(var count=0; count <= 5; count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while(count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+
+/*var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("Maior de idade.");
+}else{
+    alert("Menor de idade.");
+}*/
+
+/*var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.cor);*/
+
+/*var nome = "Fernando de Abreu";
+var idade = 21;
+alert(nome + " tem " + idade + " anos")*/
+
+/*var lista = ["maça","pêra","laranja"];
+lista.push("uva");
+lista.pop();*/
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" e "))
